1e34c51ff21a280a0af48027b0fb0473949c046f
JYM <jeanyvesmichel@yahoo.com>
2022-12-14 16:51:03 +0100

    Uppgift 2: Dra ett kort

---
+++ js/App.js
@@ -0,0 +1,43 @@
+let btn2 = document.querySelector('#btn2');
+let jsonOutput = document.querySelector('#json-output');
+
+const getJson = () => {
+    //fetch('card.json')
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=1`, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then(res => {
+            // console.log(res);
+            // if (res.ok)
+                return res.json()
+            // throw new Error('Error when fetching');
+        })
+        .then(data => {
+            console.log(data);
+
+            jsonOutput.innerHTML = "";
+
+            
+                jsonOutput.innerHTML +=
+                    `
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">${data.cards[0].code}</h3>
+                        <p class="card-text">${data.cards[0].suit}</p>
+                        <img class="align-self-start mr-3" src="${data.cards[0].image}" alt="Generic placeholder image">
+                    </div>
+                </div>
+                
+                `
+            ;
+
+        })
+        .catch(err => console.log('Error: ', err));
+
+    console.log(23);
+}
+
+btn2.addEventListener('click', getJson);
